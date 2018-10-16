f90d43962ec724e592ecd8fd443669c37306cc18
Bilal Akbar <muhammadbilalakbar021@gmail.com>
2018-10-16 06:32:19 +0500

    Delete Array_List.java

--- Array_List.java
+++
@@ -1,7 +0,0 @@
-public  interface Array_List
-{
-    int bubble_Sort(int[] array);
-    int insertion_Sort(int[] array);
-    int selection_Sort(int[] array);
-    int modifiedBubble_Sort(int[] array);
-}
